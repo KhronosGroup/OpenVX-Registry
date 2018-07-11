3a6388eba16176f716932160eb340a6adbf16aeb
fbrill3 <35230216+fbrill3@users.noreply.github.com>
2018-07-11 17:22:09 -0500

    Update IX extension spec to latest version addressing final comments.

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ include_once("../../assets/static_pages/khr_page_top.php");
   <li>OpenVX 1.1.1 export and import extension
     (<a href="extensions/vx_khr_ix/1.1.1/vx_khr_export_and_import_1_1_1.html">HTML</a>,
     <a href="extensions/vx_khr_ix/1.1.1/vx_khr_export_and_import_1_1_1.pdf">PDF</a>)
-      (updated June 17, 2018).</li>
+      (updated July 11, 2018).</li>
     <ul>
       <li>This extension designed for OpenVX 1.1 is valid without change for OpenVX 1.2(.1).</li>
     </ul>
