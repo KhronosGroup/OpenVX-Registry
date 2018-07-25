c6b34c0b93fbfa2bf01160224b602b81bcbc022e
fbrill3 <35230216+fbrill3@users.noreply.github.com>
2018-07-25 16:01:28 -0500

    Update index.php to point to new AsciiDoc locations for provisional extensions. Updated classifier, IX, import kernel, ICD, OpenCL interop, S16, and tiling.
    Not yet updated: pipelining and XML.

--- index.php
+++ index.php
@@ -53,37 +53,37 @@ include_once("../../assets/static_pages/khr_page_top.php");
 <h3> OpenVX Provisional Extensions </h3>
 <ul>
   <li>Classifier extension (provisional, OpenVX 1.2)
-    (<a href="extensions/vx_khr_class/html/index.html">HTML</a>,
-    <a href="extensions/vx_khr_class/OpenVX_Classifier_Extension_1_2.pdf">PDF</a>)
-    (updated Jul 11, 2017).</li>
+    (<a href="extensions/vx_khr_class/1.2.1/vx_khr_class_1_2_1.html">HTML</a>,
+    <a href="extensions/vx_khr_class/1.2.1/vx_khr_class_1_2_1.pdf">PDF</a>)
+    (updated July 11, 2018).</li>
   <li>Export and import extension (provisional, OpenVX 1.1)
-    (<a href="extensions/vx_khr_ix/1.0/html/index.html">HTML</a>,
-    <a href="extensions/vx_khr_ix/1.0/OpenVX_Export_and_Import_Extension_1_1.pdf">PDF</a>)
-    (updated September, 2016).</li>
+    (<a href="extensions/vx_khr_ix/1.1.1/vx_khr_export_and_import_1_1_1.html">HTML</a>,
+    <a href="extensions/vx_khr_ix/1.1.1/vx_khr_export_and_import_1_1_1.pdf">PDF</a>)
+    (updated July 11, 2018).</li>
   <li>Import kernel extension (provisional, OpenVX 1.1, 1.2)
-    (<a href="extensions/vx_khr_import_kernel/vx_khr_import_kernel.html">HTML</a>,
-    <a href="extensions/vx_khr_import_kernel/vx_khr_import_kernel.pdf">PDF</a>)
+    (<a href="extensions/vx_khr_import_kernel/1.0/vx_khr_import_kernel_1_0.html">HTML</a>,
+    <a href="extensions/vx_khr_import_kernel/1.0/vx_khr_import_kernel_1_0.pdf">PDF</a>)
     (updated March 8, 2018).</li>
   <li>Installable client driver extension (provisional, OpenVX 1.0)
-    (<a href="extensions/vx_khr_icd/html/index.html">HTML</a>,
-    <a href="extensions/vx_khr_icd/OpenVX_ICD_Extension.pdf">PDF</a>)
-    (updated September, 2016).</li>
+    (<a href="extensions/vx_khr_icd/1.0.1/vx_khr_icd_1_0_1.html">HTML</a>,
+    <a href="extensions/vx_khr_icd/1.0.1/vx_khr_icd_1_0_1.pdf">PDF</a>)
+    (updated July 11, 2018).</li>
   <li>OpenCL interop extension (provisional, OpenVX 1.1, 1.2)
-    (<a href="extensions/vx_khr_opencl_interop/vx_khr_opencl_interop.html">HTML</a>,
-    <a href="extensions/vx_khr_opencl_interop/vx_khr_opencl_interop.pdf">PDF</a>)
-    (updated Jan 25, 2018).</li>
+    (<a href="extensions/vx_khr_opencl_interop/1.0/vx_khr_opencl_interop_1_0.html">HTML</a>,
+    <a href="extensions/vx_khr_opencl_interop/1.0/vx_khr_opencl_interop_1_0.pdf">PDF</a>)
+    (updated January 25, 2018).</li>
   <li>Pipelining, streaming,and batch processing extension (provisional, OpenVX 1.1 and 1.2)
     (<a href="extensions/vx_khr_pipelining/html/index.html">HTML</a>,
     <a href="extensions/vx_khr_pipelining/OpenVX_Graph_Pipelining_Streaming_and_Batch_Processing_Extension_1_0.pdf">PDF</a>)
     (updated December, 2017).</li>
   <li>S16 extension (provisional, OpenVX 1.1)
-    (<a href="extensions/vx_khr_s16/1.1/html/index.html">HTML</a>,
-    <a href="extensions/vx_khr_s16/1.1/OpenVX_S16_Extension_1.1.pdf">PDF</a>)
-    (updated February, 2017).</li>
+    (<a href="extensions/vx_khr_s16/1.1.1/vx_khr_s16_1_1_1.html">HTML</a>,
+    <a href="extensions/vx_khr_s16/1.1.1/vx_khr_s16_1_1_1.pdf">PDF</a>)
+    (updated July 15, 2018).</li>
   <li>Tiling extension (provisional, OpenVX 1.0)
-    (<a href="extensions/vx_khr_tiling/1.0/html/index.html">HTML</a>,
-    <a href="extensions/vx_khr_tiling/1.0/OpenVX_Tiling_Extension_1_0.pdf">PDF</a>)
-    (updated January, 2015).</li>
+    (<a href="extensions/vx_khr_tiling/1.0.1/vx_khr_tiling_1_0_1.html">HTML</a>,
+    <a href="extensions/vx_khr_tiling/1.0.1/vx_khr_tiling_1_0_1.pdf">PDF</a>)
+    (updated July 19, 2018).</li>
   <li>XML schema extension (provisional, OpenVX 1.0)
     (<a href="extensions/vx_khr_xml/1.0/html/index.html">HTML</a>,
     <a href="extensions/vx_khr_xml/1.0/OpenVX_XML_Schema_Extension_1_0.pdf">PDF</a>)
